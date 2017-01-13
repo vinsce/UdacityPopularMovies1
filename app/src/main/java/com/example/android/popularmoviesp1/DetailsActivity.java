9fb60e654ef64dcefb225de2add5ba4b7bda02d5
Vincenzo Cerminara <vincenzo.cerminara94@gmail.com>
2017-01-13 15:51:54 +0100

    populating details view with movie details

--- app/src/main/java/com/example/android/popularmoviesp1/DetailsActivity.java
+++ app/src/main/java/com/example/android/popularmoviesp1/DetailsActivity.java
@@ -2,16 +2,22 @@ package com.example.android.popularmoviesp1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.popularmoviesp1.model.Movie;
+import com.example.android.popularmoviesp1.utils.APIUtils;
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
 	public static final String MOVIE_JSON_ARG_KEY = "MOVIE_JSON";
 
 	private Movie mMovie;
+
+	private TextView mTitleTextView, mPlotTextView, mDateTextView, mRatingTextView;
+	private ImageView mImageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,21 @@ public class DetailsActivity extends AppCompatActivity {
 
 		setContentView(R.layout.activity_details);
 
-		Log.d(getClass().getSimpleName(), mMovie.toString());
+		mTitleTextView = (TextView) findViewById(R.id.tv_title);
+		mPlotTextView = (TextView) findViewById(R.id.tv_plot);
+		mDateTextView = (TextView) findViewById(R.id.tv_date);
+		mRatingTextView = (TextView) findViewById(R.id.tv_rating);
+		mImageView = (ImageView) findViewById(R.id.image);
+
+		populateView();
+	}
+
+	void populateView() {
+		mTitleTextView.setText(mMovie.getTitle());
+		mPlotTextView.setText(mMovie.getPlotSynopsis());
+		mDateTextView.setText(String.valueOf(mMovie.getReleaseDate().getYear()));
+		mRatingTextView.setText(String.valueOf(mMovie.getUserRating()));
+
+		Picasso.with(this).load(APIUtils.getURLForImage(mMovie.getImageUrl())).into(mImageView);
 	}
 }
